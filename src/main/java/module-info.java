d34471882332de9a6969287f54c2fe0dff59825e
Maxim_Golostsuk <mgoloshuk@gmail.com>
2025-09-22 16:28:23 +0300

    Small steps

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.timer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.timer to javafx.fxml;
+    exports org.example.timer;
+}
---
+++ src/main/java/org/example/timer/Main.java
@@ -0,0 +1,9 @@
+package org.example.timer;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
